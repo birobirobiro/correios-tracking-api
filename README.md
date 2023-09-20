6a4a99ae52bce12627e60759107dd1f2721a2e7c
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-09-20 14:48:31 -0300

    projeto finalizado

---
+++ README.md
@@ -0,0 +1,13 @@
+<strong><h1 align="center">Correios Tracking API</h1></strong>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- Node.js
+- correios-rastreamento
+- express
+
+---
+
+Feito com ♥ by [birobirobiro](https://birobirobiro.dev)
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const correios = require('correios-rastreamento');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/:trackingCode', async (req, res) => {
+  const trackingCode = req.params.trackingCode;
+
+  try {
+    const trackingInfo = await correios.sroV2.rastrearObjeto(trackingCode);
+    res.json(trackingInfo);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch tracking data' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
